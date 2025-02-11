e574e090fcb21208d8e38b4f2a60f423d256399a
João Graciotim <graciotim92@gmail.com>
2025-02-11 19:15:50 -0300

    added properties first_name, last_name, phone, location_id and softDeletes in create_user_table migration

--- database/migrations/0001_01_01_000000_create_user_table.php
+++ database/migrations/0001_01_01_000000_create_user_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('phone');
+
+            $table->unsignedBigInteger('location_id');
+            $table->foreign('location_id')->references('id')->on('location');
+
+            $table->softDeletes();
             $table->timestamps();
         });
 
