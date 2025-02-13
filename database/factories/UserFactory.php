514d91509b4c8168c81a91062e0ffffc8610b7a7
João Graciotim <graciotim92@gmail.com>
2025-02-12 22:34:48 -0300

    added properties to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'phone' => $this->faker->phoneNumber(),
+            'location_id' => Location::factory(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
