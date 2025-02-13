0f62b02177cea9d2f586c1f8ca1ccebdb3b2afe2
João Graciotim <graciotim92@gmail.com>
2025-02-12 23:14:42 -0300

    created PreferredSpeciesFactory

---
+++ database/factories/PreferredSpeciesFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Species;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PreferredSpeciesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'species_id' => Species::inRandomOrder()->value('id'),
+            'user_id' => User::inRandomOrder()->value('id')
+        ];
+    }
+}
